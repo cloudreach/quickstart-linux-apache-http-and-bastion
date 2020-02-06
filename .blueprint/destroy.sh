bd5879e7384416754c07b08ccd6d78fa68924906
James O'Hara <jamesohara@Jamess-MacBook-Pro.local>
2020-02-06 16:07:04 +0000

    SNS for destroy

--- .blueprint/destroy.sh
+++ .blueprint/destroy.sh
@@ -13,3 +13,6 @@ aws cloudformation wait stack-delete-complete --stack-name ${STACK_NAME} \
  --region eu-west-1
 
 echo "Blueprint has been destroyed..."
+aws sns publish --topic-arn "arn:aws:sns:eu-west-1:077319295051:build-notificationa" \
+--message "Destroy of stack ${STACK_NAME} is complete." \
+--subject "Stack Destroy Complete"
